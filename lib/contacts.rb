8e7fc9e1c7abe5773a3dd51dbdc28b1e9dd3dee3
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-09 14:52:45 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,9 +17,12 @@ require 'pry'
 def remove_strawberry(contacts)
 
   contacts.each do |person, contact_details_hash|
-    binding.pry
 
+    if person == "Freddy Mercury"
+
+      contacts.each do |attribute,data|
+         binding.pry
+       end
  end
- binding.pry
 
 end
